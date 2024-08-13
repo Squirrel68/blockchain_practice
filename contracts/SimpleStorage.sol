494c421c7797de52fbf8440af1628082ce2229b3
Squirrel68 <constantlypeaceinside@gmail.com>
2024-08-13 14:34:10 +0700

    feat(lesson2): array-struct

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -4,9 +4,23 @@ pragma solidity ^0.8.8;
 contract SimpleStorage {
     uint256 public favoriteNumber;
 
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    People[] public people;
+
+
     function store (uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
     }
 
+    function retrieve() public view returns (uint256) {
+        return favoriteNumber;
+    }
 
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_favoriteNumber, _name));
+    }
 }
